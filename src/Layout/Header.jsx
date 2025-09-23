38bcb0eea39cf89f68d244a8e7ad15887f068fd2
SharpenedMindTechSolutions <sharpenmind822@gmail.com>
2025-09-23 11:10:38 +0530

    16commit

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -26,8 +26,9 @@ const Header = () => {
   };
 
   const username = localStorage.getItem('username');
-  const userId = user?._id || user?.id;
-  const userName = user?.name || username || 'user';;
+  const userId = localStorage.getItem('userId');
+  const rawName = user?.name || username || "user";
+  const userName = rawName.length > 16 ? rawName.slice(0, 16) + "..." : rawName;
   return (
     <header className="bg-black shadow-sm sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -106,7 +107,7 @@ const Header = () => {
                 </Link>
 
                 <Link
-                viewTransition
+                  viewTransition
                   to="/register"
                   className="text-black px-4 py-2 rounded-md text-sm font-medium bg-yellowCustom hover:bg-yellowCustom transition-colors"
                 >
--- src/Pages/userpage/Profilepage.jsx
+++ src/Pages/userpage/Profilepage.jsx
@@ -3,14 +3,12 @@ import Header from '../../Layout/Header'
 import Footer from '../../Layout/Footer'
 import Profile from '../../User/Profile'
 import QuickContact from '../../Components/Common/QuickContact'
-import { useParams } from 'react-router-dom'
 
 function Profilepage() {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-    
-    const { id } = useParams();
+
     return (
         <div>
             <QuickContact />
