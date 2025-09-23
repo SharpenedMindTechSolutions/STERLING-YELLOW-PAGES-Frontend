cbaf120a072c487b8d8001a0d71da1b3126f91a1
SharpenedMindTechSolutions <sharpenmind822@gmail.com>
2025-09-23 10:57:42 +0530

    15commit

--- src/Pages/userpage/Profilepage.jsx
+++ src/Pages/userpage/Profilepage.jsx
@@ -3,16 +3,19 @@ import Header from '../../Layout/Header'
 import Footer from '../../Layout/Footer'
 import Profile from '../../User/Profile'
 import QuickContact from '../../Components/Common/QuickContact'
+import { useParams } from 'react-router-dom'
 
 function Profilepage() {
-      useEffect(() => {
-                window.scrollTo(0, 0);
-            }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+    
+    const { id } = useParams();
     return (
         <div>
-            <QuickContact/>
+            <QuickContact />
             <Header />
-            <Profile/>
+            <Profile />
             <Footer />
         </div>
     )
